13fbb73eb33538ce02e0913c96faec24d62a4573
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-11 21:05:52 -0700

    the dog pages are now fully set up

--- src/DogDetail.js
+++ src/DogDetail.js
@@ -7,6 +7,13 @@ const DogDetail = ({ dogs }) => {
   return (
     <div>
       <h1>{dog.name}'s Page!</h1>
+      <img src={dog.src} />
+      <h3>Age: {dog.age}</h3>
+      <h3>Facts:
+        <ul style={{'list-style': 'none'}}>
+          {dog.facts.map(fact => <li>{fact}</li>)}
+        </ul>
+      </h3>
     </div>
   )
 };
